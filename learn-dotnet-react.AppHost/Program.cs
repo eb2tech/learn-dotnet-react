a4c7613c9ffd3ff409e7fed5fc287bc891277b09
Edward Blakes <eblakes@eb2technologies.com>
2025-05-23 02:30:11 -0500

    works but need to get ports correct

--- learn-dotnet-react.AppHost/Program.cs
+++ learn-dotnet-react.AppHost/Program.cs
@@ -3,7 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var server = builder.AddProject<Projects.learn_dotnet_react_Server>("server")
                     .WithExternalHttpEndpoints();
 
-builder.AddNpmApp("reactvite", "../learn-dotnet-react.client")
+builder.AddNpmApp("vite", "../learn-dotnet-react.client")
        .WithReference(server)
        .WaitFor(server)
        .WithEnvironment("BROWSER", "none")
