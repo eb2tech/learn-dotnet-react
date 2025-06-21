6e5cd551b157b26e836c6570deeded28aaa74961
Edward Blakes <eblakes@eb2technologies.com>
2025-06-20 20:03:02 -0500

    Minor updates

--- ReactWithDotnet.AppHost/Program.cs
+++ ReactWithDotnet.AppHost/Program.cs
@@ -2,14 +2,14 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddDockerComposeEnvironment("docker-compose");
 
-var weatherApi = builder.AddProject<Projects.ReactWithDotnet_Server>("server")
-                        .WithExternalHttpEndpoints();
+var weatherApi = builder.AddProject<Projects.ReactWithDotnet_Server>("server");
 
 builder.AddNpmApp("client", "../reactwithdotnet.client")
        .WithReference(weatherApi)
        .WaitFor(weatherApi)
        .WithHttpsEndpoint(env: "VITE_PORT")
        .WithExternalHttpEndpoints()
-       /*.PublishAsDockerFile()*/;
+       .WithOtlpExporter()
+       .PublishAsDockerFile();
 
 builder.Build().Run();
--- ReactWithDotnet.Server/Program.cs
+++ ReactWithDotnet.Server/Program.cs
@@ -5,8 +5,9 @@ builder.AddServiceDefaults();
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddProblemDetails();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -18,11 +19,8 @@ app.MapStaticAssets();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "ReactWithDotnet API V1");
-    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -32,5 +30,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.MapFallbackToFile("/index.html");
+app.MapDefaultControllerRoute();
 
 app.Run();
